18b8e66af3107e22eeb2b877b9057bd4619101cd
namin-amin <67176003+namin-amin@users.noreply.github.com>
2020-11-29 15:11:04 +0530

    v3.1
    remove cache

--- ele-code/public/electron.js
+++ ele-code/public/electron.js
@@ -18,6 +18,9 @@ function createWindow() {
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.removeMenu();
   mainWindow.on('closed', () => mainWindow = null);
+   mainWindow.webContents.session.clearCache(function(){
+    console.log("done")
+    });
 }
 
 
